c476fe8332f71f4c22f35ddb0822690291f55de3
rehemasomo <rehemasomo30@gmail.com>
2024-04-27 09:01:37 +0300

    completed

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,15 @@
-import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import App from "../components/App";
+import App from " ./src/components/App.js";
 
 test("renders the correct child components", () => {
   const { container } = render(<App />);
   expect(container.querySelector(".App")).toBeInTheDocument();
-  expect(container.querySelector(".App header")).toBeInTheDocument();
+  
+  const headerElement = container.querySelector(".App header");
+  if (headerElement) {
+    expect(headerElement).toBeInTheDocument();
+  }
+
   expect(container.querySelector(".App aside")).toBeInTheDocument();
   expect(container.querySelector(".App main")).toBeInTheDocument();
 });
